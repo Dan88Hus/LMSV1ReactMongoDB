9a8d4f586b4daef43c7146ff69bc68ca899782f2
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-06-11 12:54:06 +0300

    backend stripe

--- server/controllers/instructor.js
+++ server/controllers/instructor.js
@@ -1,29 +1,34 @@
 const User = require('../models/user')
-const stripe = require("stripe")
+const stripe = require("stripe")(process.env.STRIPE_SECRET)
 const queryString = require("query-string")
 
 
 
 exports.makeInstructor = async(req, res)=>{
     try {
+        console.log("============>we are in")
         const user = await User.findById(req.user._id).exec()
         if(!user.stripe_account_id){
             const account = await stripe.accounts.create({type: "express"})
-            user.stripe_account_id = account._id 
+            // console.log(account.id)
+            
+            user.stripe_account_id = account.id 
             user.save()
         }
-        const accountLink = await stripe.accountLinks.create({
+        let accountLink = await stripe.accountLinks.create({
             account: user.stripe_account_id,
             refresh_url: process.env.STRIPE_REDIRECT_URL,
             return_url: process.env.STRIPE_REDIRECT_URL,
             type: "account_onboarding",
         })
+        // console.log("here we are")
+        // console.log("accountLink====", accountLink)
         accountLink = Object.assign(accountLink, {
             "stripe_user[email]": user.email,
         })
-        //query-string arrages key: value as url address
+        //query-string arranges key: value as url address
         return res.json(`${accountLink.url}?${queryString.stringify(accountLink)}`)
     } catch (error) {
-        console.log(error)
+        // console.log(error)
     }   return res.status(400).send("make instructor , stripe error")
 }
