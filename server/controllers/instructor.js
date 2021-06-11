e0af64992e68f700372e19ea4c0e4f990245a800
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-06-11 12:22:04 +0300

    stripe onboarding server

--- server/controllers/instructor.js
+++ server/controllers/instructor.js
@@ -1,16 +1,29 @@
 const User = require('../models/user')
+const stripe = require("stripe")
+const queryString = require("query-string")
+
 
 
 exports.makeInstructor = async(req, res)=>{
     try {
-        const {email, code, newPassword} = req.body
-        // console.table( req.body)
-        const hashedPassword = await hashPassword(newPassword)
-        const user = User.findOneAndUpdate({
-            email, passwordResetCode: code
-        }, {password: hashedPassword, passwordResetCode: ""}).exec()
-        return res.json({ok: true})
+        const user = await User.findById(req.user._id).exec()
+        if(!user.stripe_account_id){
+            const account = await stripe.accounts.create({type: "express"})
+            user.stripe_account_id = account._id 
+            user.save()
+        }
+        const accountLink = await stripe.accountLinks.create({
+            account: user.stripe_account_id,
+            refresh_url: process.env.STRIPE_REDIRECT_URL,
+            return_url: process.env.STRIPE_REDIRECT_URL,
+            type: "account_onboarding",
+        })
+        accountLink = Object.assign(accountLink, {
+            "stripe_user[email]": user.email,
+        })
+        //query-string arrages key: value as url address
+        return res.json(`${accountLink.url}?${queryString.stringify(accountLink)}`)
     } catch (error) {
         console.log(error)
-    }   return res.status(400).send("reset password error")
+    }   return res.status(400).send("make instructor , stripe error")
 }
