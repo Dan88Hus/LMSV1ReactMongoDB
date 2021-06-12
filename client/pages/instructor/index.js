e25ccc4c5ac71642e4189492ba9987d47d54e402
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-06-12 20:50:10 +0300

    courses.map

--- client/pages/instructor/index.js
+++ client/pages/instructor/index.js
@@ -1,6 +1,8 @@
 import {useEffect, useState} from 'react'
 import axios from 'axios'
 import InstructorRoute from '../../components/routes/InstructorRoute'
+import {Avatar} from "antd"
+import Link from 'next/link'
 
 const InstructorIndex = () =>{
 
@@ -13,7 +15,39 @@ const InstructorIndex = () =>{
     return (
         <InstructorRoute>
         <h1 className="text-center">Instructor Dashboard</h1>
-        {JSON.stringify(courses,null,4)}
+        {/* {JSON.stringify(courses,null,4)} */}
+        {courses && courses.map(course =>{
+            return (
+                <>
+                <div className="form-control mb-2 media pt-2">
+                    <Avatar shape="square" size={80}
+                    src={course.image ? course.image.Location : "No Image"} />
+                    <div className="media-body pl-2">
+                    <div className="row">
+                        <div className="col">
+                            <Link href={`/instructor/course/view/{course._id}`}>
+                                <a className="h5 mt-2 text-primary">{course.name}</a>
+                            </Link>
+                            <p >{course.lessons.length} Lessons</p>
+
+                            {course.lessons.legth<5 ? (<p>
+                                At least 5 lessons are required to publish a course
+                            </p>) : course.published ? (<p>
+                                Your course is live in the marketplace
+                            </p>) : (<p>Your course is ready to be published</p>) }
+                        </div>
+                    </div>
+                    <div className="col mt-3 text-end">
+                        {course.published ? (<div>Published</div>)
+                         : 
+                        (<div>Not Published</div>)}
+                    </div>
+                    </div>
+                </div>
+                </>
+
+            )
+        })}
         </InstructorRoute>
     )
 }
