56830e1b596e5d53cc30f436bc0099e794d29126
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-06-13 15:27:08 +0300

    Modal completed

--- client/pages/instructor/course/view/[slug].js
+++ client/pages/instructor/course/view/[slug].js
@@ -2,10 +2,13 @@ import {useState, useEffect} from 'react'
 import {useRouter} from 'next/router'
 import InstructorRoute from '../../../../components/routes/InstructorRoute'
 import axios from 'axios'
-import {Avatar} from 'antd'
+import {Avatar, Tooltip, Button, Modal} from 'antd'
+import {EditOutlined, CheckOutlined} from '@ant-design/icons'
+import ReactMarkDown from 'react-markdown'
 
 const CourseView = ()=>{
     const [course,setCourse] = useState({})
+    const [visible, setVisible] = useState(false)
 
     const router = useRouter()
     const {slug} = router.query
@@ -24,30 +27,60 @@ const CourseView = ()=>{
                 {course && 
                 <div className="form-control mb-2">
                     <div className="row">
-                        <div className="col">
+                        <div className="col-auto">
                         <Avatar shape="square" size={80}
                     src={course.image ? course.image.Location : "No Image"} 
                     />
                         </div>
-                        
                     <div className="col">
                     <div className="row">
-                        <div>
-                            <h5 className="text-primary text-start">{course.name}</h5>
+                        <div className="col-auto">
+                            <h5 className="text-primary text-start">Category: {course.category}</h5>
                             <p>{course.lessons && course.lessons.length} Lessons</p>
                         </div>
-                    </div>
-                    <div className="col text-end">
+                        <div className="col text-end">
                         {course.published ? (<div className="text-muted">Published</div>)
                          : 
                         (<div className="text-muted">Not Published</div>)}
-                    </div>
+                   
+                   <div className="text-end">
+                        <Tooltip title="Edit">
+                            <EditOutlined style={{cursor: "pointer"}} className="h5 mr-4"/>
+                        </Tooltip>
+                        <Tooltip title="Publish">
+                            <CheckOutlined style={{cursor: "pointer"}} className="h5 ml-4"/>
+                        </Tooltip>
                     </div>
                 </div>
+                </div>
+                </div>
+                </div>
 
+                    <div className="row">
+                        <hr style={{marginTop: "3px", width: "80%"}}/>
+                        <ReactMarkDown children={course.description}/>
                     </div>
-
+                    <Button
+                    onClick={()=> setVisible(true)}
+                    clasName="col-md-6 offset-md-3 text-center"
+                    type="primary"
+                    shape="round"
+                    size="large"
+                    >
+                        Add Lesson
+                    </Button>
+                    <Modal
+                    title="Add Lesson"
+                    centered 
+                    visible={visible}
+                    onCancel={()=> setVisible(false)}
+                    footer={null}
+                    >
+                        show add lesson component
+                    </Modal>
+                    </div>
                     }
+                    {/* {JSON.stringify(course.description)} */}
             </div>
         </InstructorRoute>
     )
