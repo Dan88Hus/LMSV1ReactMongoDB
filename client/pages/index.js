b9d97ebe030df8ef2070deb73607d03ab4b0e9a2
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-06-19 16:08:40 +0300

    Single product fetch

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,13 +6,16 @@ import {List , Avatar, Card, Badge} from 'antd'
 
 const {Item} = List
 
- const Index = () => {
-    const [courses, setCourses] = useState([])
+const Index = ({courses}) => {
+     //  const Index = () => {
+
+    // const [courses, setCourses] = useState([])
     
-    useEffect( async()=>{
-        const {data} = await axios.get("/api/courses")
-        setCourses(data)
-    },[])
+    // useEffect( async()=>{
+    //     const {data} = await axios.get("/api/courses")
+    //     setCourses(data)
+    // },[]) these are commented to maake it SEO and written new async funct at below
+    //props olarak ta{courses} i alacak
     
      return (
     <>
@@ -32,5 +35,20 @@ const {Item} = List
 
     </>
 )
-     }
+}
+
+export async function getServerSideProps() {
+    //burda API in yolunu axios a tam vermemiz gelekiyor ve env dosyasinda prefix olarak NEXT_PUBLIC_ eklemiyoruz cunku direk server ile baglanti kuruyor,
+    //  env dosyasinda PREFIX verirsek client olarak aligliyor, ama biz direk server ile baglanti kuruyoruz SEO da
+    // const {data} = await axios.get("/api/courses....... yazardik normalde ")
+    const {data} = await axios.get(`${process.env.API}/courses`)
+    return { 
+        props : {
+            courses: data
+        }
+    }
+    // now if we check page sources we can find each courses
+
+
+}
 export default Index
