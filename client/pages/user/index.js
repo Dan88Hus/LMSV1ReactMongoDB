a6d2c802c45c70c752f741f9713e9d2f35bc1f09
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-06-21 11:41:32 +0300

    client-side get all courses

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,17 +1,30 @@
-import { useState, useContext} from 'react'
+import {  useEffect, useState, useContext} from 'react'
 import {Context} from '../../context/index'
 import UserRoute from '../../components/routes/UserRoute'
+import axios from 'axios'
+
 
 const UserIndex = () => {
     const {state: {user}, dispatch} = useContext(Context)
+    const [courses, setCourses] = useState([])
+
     // state
-        
+
+    useEffect(() => {
+        loadCourses()
+    })
+    
+    const loadCourses = async() => {
+        const {data} =  await axios.get("/api/user-courses") 
+        setCourses(data)
+    }
 
     return (
     <UserRoute>
     <h1 className="text-center">
         User Dashboard
     </h1>
+    {JSON.stringify(courses)}
     </UserRoute>
     ) 
 }
